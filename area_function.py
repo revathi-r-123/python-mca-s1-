08f17b22fa68ecd58b7ca78c5de24d3079e10b95
Revathi R Menon <revathirmenon3@gmail.com>
2025-10-09 11:56:38 +0530

    Add files via upload

---
+++ area_function.py
@@ -0,0 +1,11 @@
+# lambda function
+
+area_square = lambda a:a*a
+area_rectangle = lambda l,b:l*b
+area_triangle = lambda b,h:0.5*b*h
+result1=area_square(2)
+result2=area_rectangle(2,4)
+result3=area_triangle(3,6)
+print(result1)
+print(result2)
+print(result3)
