cb9fb11b9a0b2951226c15537dd861b9aeda62d5
Revathi R Menon <revathirmenon3@gmail.com>
2025-10-23 23:06:45 +0530

    Add files via upload

---
+++ leap_year.py
@@ -0,0 +1,13 @@
+current_year = 2025 
+final_year = int(input("Enter the final year: "))
+
+if final_year == current_year:
+    print("You entered the current year. No future leap years to display.")
+elif final_year < current_year:
+    print("Please enter a year greater than the current year.")
+else:
+    print("Leap years from", current_year, "to", final_year, "are:")
+    while current_year <= final_year:
+        if (current_year % 4 == 0 and current_year % 100 != 0) or (current_year % 400 == 0):
+            print(current_year)
+        current_year += 1
