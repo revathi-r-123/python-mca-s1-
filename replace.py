2434232cb7bb5b3392d1902c4789cf84fe4d1456
Revathi R Menon <revathirmenon3@gmail.com>
2025-10-26 01:02:19 +0530

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,5 +1,10 @@
-str1="onion"
-char=str1[0]
-str1=str1.replace(char,'$')
-str1=char+str1[1:]
-print(str1)
+str1=input("Enter a string:")
+if str1:
+    char=str1[0]
+    str1=str1.replace(char,'$')
+    str1=char+str1[1:]
+    print("String:",str1)
+else:
+    print("Empty string eneterd!")
+
+
